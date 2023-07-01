3a561d5ea5c10282611ca2f659c2b3d0897c449c
Merel Jacobs <merel.burleigh@gmail.com>
2023-07-02 09:07:47 +1000

    working table of contents

--- Develop/README.md
+++ Develop/README.md
@@ -1,1 +1,8 @@
-<md><h1>Matt is hot</h1><p>[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)</p><h2>Description</h2><p>aerg</p><h2>Installation</h2><p>aerg</p><h2>Usage</h2><p>aerg</p><h2>Contribution</h2><p>aerg</p><h2>Test Instructions</h2><p>aerg</p><h2>License</h2><p>MIT permissions with usage of this code, permitting users to freely use, modify, and distribute this code with limited libility.</p><h2>Contact Information</h2><p>[GitHub Profile](https://github.com/MerelJac)</p><p>merel.burleigh@gmail.com</p></md>
+<md><h1>aerg</h1><p>[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)</p><h2>Description</h2><p>aerg</p><h2>Table of Contents</h2><ul>
+    <li><a href="#installation">Installation</a></li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#contribution">Contribution</a></li>
+    <li><a href="#test">Test Instructions</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li></ul>
+    <h2 id="installation">Installation</h2><p>aerg</p><h2 id="usage">Usage</h2><p>aerg</p><h2 id="contribution">Contribution</h2><p>aerg</p><h2 id="test">Test Instructions</h2><p>aerg</p><h2 id="license">License</h2><p>MIT permissions with usage of this code, permitting users to freely use, modify, and distribute this code with limited libility.</p><h2 id="contact">Contact</h2><p>Please contact for questions, suggestions or comments about this application. [GitHub Profile](https://github.com/aerg)</p><p>For further questions, please contact me via email: aerg</p></md>
--- Develop/index.js
+++ Develop/index.js
@@ -58,7 +58,14 @@ function writeToFile(answers, data) {
         license = "zLIB permissions with usage of this code, permitting users to freely use, modify, and distribude with limited libability.";
     }
 
-    var md = `<md><h1>${answers.title}</h1><p>${icon}</p><h2>Description</h2><p>${answers.description}</p><h2>Installation</h2><p>${answers.installation}</p><h2>Usage</h2><p>${answers.usage}</p><h2>Contribution</h2><p>${answers.contribution}</p><h2>Test Instructions</h2><p>${answers.test}</p><h2>License</h2><p>${license}</p><h2>Contact Information</h2><p>[GitHub Profile](https://github.com/${answers.gitHub})</p><p>${answers.email}</p></md>`
+    var md = `<md><h1>${answers.title}</h1><p>${icon}</p><h2>Description</h2><p>${answers.description}</p><h2>Table of Contents</h2><ul>
+    <li><a href="#installation">Installation</a></li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#contribution">Contribution</a></li>
+    <li><a href="#test">Test Instructions</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li></ul>
+    <h2 id="installation">Installation</h2><p>${answers.installation}</p><h2 id="usage">Usage</h2><p>${answers.usage}</p><h2 id="contribution">Contribution</h2><p>${answers.contribution}</p><h2 id="test">Test Instructions</h2><p>${answers.test}</p><h2 id="license">License</h2><p>${license}</p><h2 id="contact">Contact</h2><p>Please contact for questions, suggestions or comments about this application. [GitHub Profile](https://github.com/${answers.gitHub})</p><p>For further questions, please contact me via email: ${answers.email}</p></md>`
     fs.writeFile('README.md', md, (err) => {
         err ? console.error(err) : console.log('successful')
     })
